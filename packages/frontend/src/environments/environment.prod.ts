3df51782d8b4cd162f48474cedb183db067b56d5
José Antonio <lopezrosique97@gmail.com>
2026-02-20 21:50:48 +0100

    Remove environment files from Git tracking

--- packages/frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: '/api', // En producción usamos rutas relativas
-};
--- packages/frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api', // En local apuntamos al puerto 3000
-};
